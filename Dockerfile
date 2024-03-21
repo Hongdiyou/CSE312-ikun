ce829e812875bc2068b10684bcc87896368f1e60
JiaHao Wu <jwu222@buffalo.edu>
2024-03-21 14:10:53 -0400

    fix the problem of server shutting down immidetly after run

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM python:3.8
 
-ENV HOME /root
-WORKDIR /root
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+EXPOSE 8080
+
 COPY . .
 
-RUN pip3 install -r requirements.txt
-
-EXPOSE 8000
-
-ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
-RUN chmod +x /wait
-
-CMD /wait && python3 -u app.py
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, send_from_directory
 #这是我们使用的框架“Flask" 如果你们看到编译器错误需要首先安装环境
-app=Flask(__name__)
+
 
 
 #这是flask框架添加route的方式和我们作业的add_route类似，例如这里的'/'对应了我们作业的'/' 也是root path
 #这会response html文件.
+
+
+app = Flask(__name__)
+
+
+
 @app.route('/')
-def responsetoroot():
-    response= Response("/src/index.html","X-Content-Type-Options: nosniff")
-    print(response)
-    return response
+def index():
+    return render_template('index.html')
 
+@app.route('/static/img/<path:path>')
+def send_img(path):
+    return send_from_directory('static/img', path)
 
-def __main__():
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
     #ikun,启动！
